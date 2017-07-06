c690e3e632f47720f83e5ef48529e340733c0ca3
fix-fake <streepjescode@gmail.com>
2017-07-06 08:30:04 +0200

    Update cnn.js
    Added a word list

--- cnnplugin/cnn.js
+++ cnnplugin/cnn.js
@@ -1,11 +1,19 @@
-fix_skcollubs = function(obj) {
+fix_skcollubs = function(obj, words) {
     for (var p in obj) {
         if (obj[p].innerHTML) {
-            obj[p].innerHTML = obj[p].innerHTML.replace(/FraudNewsCNN/ig, 'CNN')
-            console.log(obj[p].innerHTML)
+            fix_skscollubs_words(obj[p], words)
         }
     }
 }
 
-fix_skcollubs(document.getElementsByClassName('contents'))
-fix_skcollubs(document.getElementsByClassName('quote'))
+fix_skscollubs_words = function(obj, words) {
+    words.forEach(function(word) {
+        this.innerHTML = this.innerHTML.replace(new RegExp(word, 'ig'), 'CNN')
+    }, obj)
+}
+words = [
+    'FraudNewsCNN',
+    'CrapNewsNetwork',
+]
+fix_skcollubs(document.getElementsByClassName('contents'), words)
+fix_skcollubs(document.getElementsByClassName('quote'), words)
